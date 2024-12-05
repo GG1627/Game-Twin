153a2e2d606d29ae264a4829db0d23c6331512bc
John Lewis <144287365+broncosfan915@users.noreply.github.com>
2024-12-04 23:22:36 -0500

    Added comments to similar.cpp

--- Similar.cpp
+++ Similar.cpp
@@ -1,5 +1,6 @@
 #include "Similar.h"
 
+// searches the game vector to see if the user inputted name is in it, and if true, returns the game object
 Game Similar::findGame(string name){
     for(const auto& game : gamesRef) {
         if(game.name == name) {
@@ -9,10 +10,12 @@ Game Similar::findGame(string name){
     throw runtime_error("Game not found!");
 }
 
+// returns a pair of unordered_sets where the first in the pair is the genres, and the second is the platforms
 pair<unordered_set<string>, unordered_set<string>> Similar::getParameters(string gameName) {
     return {findGame(gameName).gameGenres, findGame(gameName).gamePlatforms};
 }
 
+// filters down the original vector by first removing games that don't have similar genres, then games that are not on the selected platform
 vector<Game> Similar::extractParameters(string gameName, string gamePlatform) {
     vector<Game> filteredGames;
     pair<unordered_set<string>, unordered_set<string>> targetParameters = getParameters(gameName);
@@ -27,6 +30,7 @@ vector<Game> Similar::extractParameters(string gameName, string gamePlatform) {
         }
     }
 
+    // takes the partially extracted vector, and then further filters it, now by platform
     vector<Game> platformFilteredGames;
     for (int i = 0; i < filteredGames.size(); i++) {
         unordered_set<string> currPlatforms = getParameters(filteredGames[i].name).second;
@@ -35,13 +39,14 @@ vector<Game> Similar::extractParameters(string gameName, string gamePlatform) {
             platformFilteredGames.push_back(filteredGames[i]);
         }
     }
-
     return platformFilteredGames;
 }
 
+// fills in the values for the achievment difference for every game in the filtered array
 void Similar::findDifference(std::string gameName, vector<Game>& similarGames) {
     for(auto& i : similarGames) {
         if (i.name != gameName) {
+            // takes the absolute value of the difference of achievementCounts, to show the games that have the closest counts
             i.achievementDifference = abs(i.achievementCount - findGame(gameName).achievementCount);
         }
     }
