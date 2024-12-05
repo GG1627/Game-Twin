315173b0c7cac0bbfe3acbafda71bb4212500120
John Lewis <144287365+broncosfan915@users.noreply.github.com>
2024-12-04 23:25:11 -0500

    Updated comments for SortingAlgorithms.h

--- SortingAlgorithms.h
+++ SortingAlgorithms.h
@@ -2,18 +2,18 @@
 
 class SortingAlgorithms {
 public:
-    // main sorting algorithms
+    // metacritic sorts: both cocktail and quick sort
     void cocktailSortMetacritic(vector<Game>& games, int size);
     void quickSortMetacritic(vector<Game>& games, int low, int high);
     int partitionMetacritic(vector<Game>& games, int low, int high);
 
-    // optional sorting algorithms
+    // Achievement Count sorts: both cocktail and quick sort
     void cocktailSortAchievementCount(vector<Game>& games, int size);
     void quickSortAchievmentCount(vector<Game>& games, int low, int high);
     int partitionAchievmentCount(vector<Game>& games, int low, int high);
 
+    // Suggestion Count sorts: both cocktail and quick sort
     void cocktailSortSuggestionsCount(vector<Game>& games, int size);
     void quickSortSuggestionsCount(vector<Game>& games, int low, int high);
     int partitionSuggestionsCount(vector<Game>& games, int low, int high);
-
 };
